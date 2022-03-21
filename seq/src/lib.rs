b88535703e14933115960dd090ed1d932e445f97
ChuWenFeng <1721306020@qq.com>
2022-03-21 18:53:48 +0800

    solved seq all

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -16,6 +16,12 @@ impl syn::parse::Parse for SeqParser{
         let start: syn::LitInt = input.parse()?;
 
         input.parse::<Token!(..)>()?;
+        
+        let mut inc = false;
+        if input.peek(syn::Token!(=)){
+            input.parse::<syn::Token!(=)>()?;
+            inc = true;
+        }
 
         let end:syn::LitInt = input.parse()?;
 
@@ -24,12 +30,16 @@ impl syn::parse::Parse for SeqParser{
 
         let body: proc_macro2::TokenStream = body_buf.parse()?;
 
-        let t = SeqParser{
+        let mut t = SeqParser{
             variable_ident,
             start:start.base10_parse()?,
             end:end.base10_parse()?,
             body,
         };
+
+        if inc{
+            t.end +=1;
+        }
 
         Ok(t)
     }
@@ -88,6 +98,70 @@ impl SeqParser{
         }
         ret
     }
+
+    fn find_block_to_expand_and_to_expand(&self,c:syn::buffer::Cursor)->(proc_macro2::TokenStream,bool){
+        let mut found = false;
+        let mut ret = proc_macro2::TokenStream::new();
+
+        let mut cursor = c;
+
+        while !cursor.eof(){
+            if let Some((punct_prefix,cursor_1)) = cursor.punct(){
+                if punct_prefix.as_char() == '#'{
+                    if let Some((group_cur,_,cursor_2)) = cursor_1.group(proc_macro2::Delimiter::Parenthesis){
+                        if let Some((punct_suffix,cursor_3)) = cursor_2.punct(){
+                            if punct_suffix.as_char() == '*'{
+                                for i in self.start..self.end{
+                                    let t = self.expand(&group_cur.token_stream(), i);
+                                    ret.extend(t);
+                                }
+                                cursor = cursor_3;
+                                found = true;
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if let Some((group_cur,_,next_cur)) = cursor.group(proc_macro2::Delimiter::Brace){
+                let (t,f) = self.find_block_to_expand_and_to_expand(group_cur);
+                found = f;
+                ret.extend(quote::quote!( {#t} ));
+                cursor = next_cur;
+                continue;
+            }else if let Some((group_cur,_,next_cur)) = cursor.group(proc_macro2::Delimiter::Bracket){
+                let (t,f) = self.find_block_to_expand_and_to_expand(group_cur);
+                found = f;
+                ret.extend(quote::quote!( [#t] ));
+                cursor = next_cur;
+                continue;
+            }else if let Some((group_cur,_,next_cur)) = cursor.group(proc_macro2::Delimiter::Parenthesis) {
+                let (t,f) = self.find_block_to_expand_and_to_expand(group_cur);
+                found = f;
+                ret.extend(quote::quote!( (#t) ));
+                cursor = next_cur;
+                continue;
+            }else if let Some((punct,next_cur)) = cursor.punct() {
+                ret.extend(quote::quote! (#punct));
+                cursor = next_cur;
+                continue;
+            }else if let Some((ident,next_cur)) = cursor.ident(){
+                ret.extend(quote::quote!(#ident));
+                cursor = next_cur;
+                continue;
+            } else if let Some((literal,next_cur)) = cursor.literal() {
+                ret.extend(quote::quote! (#literal));
+                cursor = next_cur;
+                continue;
+            }else if let Some((lifetime,next_cur)) = cursor.lifetime(){
+                ret.extend(quote::quote! (#lifetime));
+                cursor = next_cur;
+                continue;
+            }
+        }
+        return (ret,found);
+    }
 }
 
 #[proc_macro]
@@ -97,6 +171,14 @@ pub fn seq(input: TokenStream) -> TokenStream {
     // eprintln!("st.body:{}",st.body);
 
     let mut ret = proc_macro2::TokenStream::new();
+
+    let buffer = syn::buffer::TokenBuffer::new2(st.body.clone());
+
+    let (ret_1,expand) = st.find_block_to_expand_and_to_expand(buffer.begin());
+    if expand{
+        return ret_1.into();
+    }
+
     for i in st.start..st.end{
         ret.extend(st.expand(&st.body, i));
     }
